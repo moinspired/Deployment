42b2d92ab1ababcda159a697a39503ac8afa038a
Mo <motuma.ayana1@gmail.com>
2022-08-09 14:45:22 -0300

    Removed Lines

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-  res.send('Hello World2!')
-  res.send('Hello World3!')
-  res.send('Hello World4!')
 })
 
 app.listen(port, () => {
